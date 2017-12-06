dab2632426638e5396b623d9d4f0a69b15548918
Anisha Miah <31706730+Anishamiah@users.noreply.github.com>
2017-12-06 13:29:25 -0500

    Add files via upload

---
+++ Exercise 7.py
@@ -0,0 +1,19 @@
+print("Exercise 7a) \n:");
+
+list= [1,2]
+
+print ("List:", list);
+
+print("Exercise 7b) \n:");
+
+L= [1,2]
+x=L[::-1]
+print("Reversed list:",x);
+
+print("Exercise 7c) \n:");
+
+L= [1,2]
+count=0
+for i in L:
+    count=count+1
+print("The number of elements in the list:",count);
