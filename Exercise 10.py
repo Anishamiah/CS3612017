f6f2ebc32a0dd4a32d0a43fdc897e1cb642f284a
Anisha Miah <31706730+Anishamiah@users.noreply.github.com>
2017-12-12 18:24:06 -0500

    Add files via upload

---
+++ Exercise 10.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import math
+import matplotlib.pyplot as plt
+
+def f(x):
+    answer = math.pow(math.sin(x-2),2) * math.pow(math.e,- (math.pow(x,2)));
+    return answer;
+
+
+input = [];
+output = [];
+
+
+iter = 0;
+
+while iter <= 2:
+    input.append(iter);
+    iter+= 0.08;
+
+iter = 0;
+
+
+while iter < len(input):
+    output.append(f(input[iter]));
+    iter += 1;
+
+print (output);
+print(math.pi);
+plt.xlabel("X-Axis");
+plt.ylabel("Y-Axis");
+plt.title("sin^2(x-2)e^(-x^2)");
+plt.ylim(0,1.5);
+plt.xlim(0,2.5);
+plt.plot(input,output);
+plt.show()
+
+
+
